6d63660364ed439778a66b527250b05ad0e8caad
chounjae <blueskycuj@naver.com>
2026-02-13 19:37:51 +0900

    [#13]; chore: mvc 설정파일 추가

---
+++ src/main/kotlin/org/example/beyondubackend/common/config/WebMvcConfig.kt
@@ -0,0 +1,14 @@
+package org.example.beyondubackend.common.config
+
+import org.example.beyondubackend.common.resolver.ExamScoreArgumentResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(ExamScoreArgumentResolver())
+    }
+}
