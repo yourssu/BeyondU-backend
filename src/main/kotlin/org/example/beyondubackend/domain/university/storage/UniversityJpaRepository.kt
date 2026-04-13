9d7e751462bea195c6a2465221f5a9833acf5329
chounjae <blueskycuj@naver.com>
2026-04-13 17:05:14 +0900

    [#43]; feat: region(nations)으로 응답

--- src/main/kotlin/org/example/beyondubackend/domain/university/storage/UniversityJpaRepository.kt
+++ src/main/kotlin/org/example/beyondubackend/domain/university/storage/UniversityJpaRepository.kt
@@ -1,5 +1,9 @@
 package org.example.beyondubackend.domain.university.storage
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface UniversityJpaRepository : JpaRepository<UniversityEntity, Long>
+interface UniversityJpaRepository : JpaRepository<UniversityEntity, Long> {
+    @Query("SELECT DISTINCT u.region, u.nation FROM UniversityEntity u ORDER BY u.region, u.nation")
+    fun findDistinctRegionAndNation(): List<Array<String>>
+}
