8a7b5eaf4a82cc0ef0db9468763deb3bcde1b855
chounjae <blueskycuj@naver.com>
2026-04-25 21:14:06 +0900

    [#47]; fix: 카테고리 오류 수정

--- src/main/kotlin/org/example/beyondubackend/common/enums/MajorCategory.kt
+++ src/main/kotlin/org/example/beyondubackend/common/enums/MajorCategory.kt
@@ -3,7 +3,9 @@ package org.example.beyondubackend.common.enums
 enum class MajorCategory(val displayName: String) {
     BUSINESS("경영/경제 분야"),
     IT_ENGINEERING("IT/공학 분야"),
-    HUMANITIES_SOCIAL_LAW("인문/사회/법학 분야"),
-    SCIENCES_ARTS("자연과학/예술/기타 분야"),
+    HUMANITIES_SOCIAL("인문/사회 분야"),
+    LAW("법학 분야"),
+    NATURAL_SCIENCES("자연과학 분야"),
+    ARTS("예술 분야"),
     OTHER("기타"),
 }
